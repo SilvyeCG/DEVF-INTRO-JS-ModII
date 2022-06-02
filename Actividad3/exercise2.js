ba4e348406050a77c8d12c359320597eab748b99
silvye <scortesg9527@gmail.com>
2022-06-01 21:20:57 -0500

    act 3 ej2 corregidos

--- Actividad3/exercise2.js
+++ Actividad3/exercise2.js
@@ -34,24 +34,14 @@ if(player1 === player2) {
     console.log('Empate');
 }else if(player1 == 'piedra' && player2 == 'papel'){
     console.log('El papel envuelve la piedra, papel gana')
-
 }else if(player1 =='papel' && player2 == 'piedra'){
     console.log('El papel envuelve la piedra, papel gana')
-
 }else if(player1 == 'tijera' && player2 == 'papel'){
     console.log('Las tijeras cortan al papel, tijeras ganan');
-
 }else if(player1 =='papel' && player2 == 'tijera'){
     console.log('Las tijeras cortan al papel, tijeras ganan');
-
 }else if(player1 == 'piedra' && player2 == 'tijera'){
     console.log('La piedra rompe las tijeras, piedra gana');
-
 }else if(player1 =='tijera' && player2 == 'piedra'){
     console.log('La piedra rompe las tijeras, piedra gana');
-
 }
-else{
-    console.log("¿Seguro que estamos jugando piedra, papel o tijera?")
-    
-} 
