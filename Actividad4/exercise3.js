b346464a23ba8ac1780d9979b8e968277ca5b0e7
silvye <scortesg9527@gmail.com>
2022-06-02 20:08:51 -0500

    Documentado

--- Actividad4/exercise3.js
+++ Actividad4/exercise3.js
@@ -1,3 +1,7 @@
+/*
+Crea el mismo problema 2, ahora usando Do While
+*/
+
 let i = 0;
 var num = prompt('Introduce un numero');
 
--- Actividad4/exercise4.js
+++ Actividad4/exercise4.js
@@ -1,5 +1,7 @@
+/*
+Usando for, crea un programa que imprima en consola los números impares del 1 al 50
 
-var i = 0;
+*/
 
 for(var i = 1; i <= 50; i++){
     if(i % 2 != 0){
