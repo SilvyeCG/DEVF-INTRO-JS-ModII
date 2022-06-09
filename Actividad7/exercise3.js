7b22786af044e7133cad251ea4e818457783bae3
silvye <scortesg9527@gmail.com>
2022-06-09 13:24:12 -0500

    Practice 7.3 correct

--- Actividad7/exercise3.js
+++ Actividad7/exercise3.js
@@ -5,6 +5,29 @@ const word1 = 'madam' const word2 = 'computadora'
 
 */
 
+/* Esta no funcionó
+
+function palindromo(palabra){
+
+    //The split() method splits a string into an array of substrings.
+    let array = palabra.split(' ');
+
+    //The reverse() method reverses an array in place. The first array element becomes the last, and the last array element becomes the first.
+    let reverse = array.reverse();
+
+    //El método join() une todos los elementos de una matriz (o un objeto similar a una matriz) en una cadena y devuelve esta cadena.
+
+    let join = reverse.join();
+
+    if(palabra == join){
+        console.log('Palabra palindromo');
+    }else{
+        console.log('No es un palindromo');
+    } 
+}
+*/
+
+
 function palindromo(palabra){
     let arr = palabra.split(' ');
     let reverse = arr.reverse();
@@ -14,8 +37,9 @@ function palindromo(palabra){
 
 }
 
-// word = prompt("introduce una palabra");
 palindromo("madam");
 palindromo("computadora");
+
+
 
 
