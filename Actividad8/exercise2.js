580d83796a4fbbeff79b5e83722109774672dbb2
silvye <scortesg9527@gmail.com>
2022-06-09 15:44:09 -0500

    Act 82 updated

--- Actividad8/exercise2.js
+++ Actividad8/exercise2.js
@@ -9,16 +9,18 @@ Objeto de muestra:
     };
  */
 
-    var student = {
-        name: "David Rayy",
-        sclass: "VI",
-        rollno: 12
-    };
+var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+};    
 
-    for(let i = 0; i < Object.entries(student); i++){
-        console.log(`$[i]: ${key}: ${value}`)
-    }
+//El método Object.entries() devuelve una matriz de pares propios de una propiedad enumerable [key, value] de un objeto dado
 
-    // for (var [key, value] of Object.entries(student)) {
-    //     console.log(`${key}: ${value}`);
-    //   }
+for(let i = 0; i < Object.entries(student); i++){
+    console.log(`$[i]: ${key}: ${value}`)
+}
+
+// for (var [key, value] of Object.entries(student)) {
+//     console.log(`${key}: ${value}`);
+//   }
