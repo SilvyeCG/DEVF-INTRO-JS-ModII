0551f9b8ca1f33007dbbbb3aa8c2711e21e8b1d5
silvye <scortesg9527@gmail.com>
2022-06-09 16:49:20 -0500

    Act 9.1

--- Actividad9/exercise1.js
+++ Actividad9/exercise1.js
@@ -1,32 +1,25 @@
 /*
  * Escriba una función de JavaScript que tomará una serie de números almacenados
- * y encontrará el segundo números más bajos y segundos más altos, respectivamente
+ * y encontrará el segundo número más bajos y segundo más altos, respectivamente
  * del siguiente arreglo:  const arr = [3,4,6,1,5,2,9,10,23,12]
  */
 
 const arr = [3,4,6,1,5,2,9,10,23,12];
-var maximo = 0
-var minimo = 0
-
 
 /*
 
-function myFunction(){
-
-    for(var i = 0; i < arr.length; i++){
-
-        if(maximo < arr[i]){
-            maximo = arr[i];
-        }
-
-        if(minimo > arr[i]){
-            minimo = arreglo[i];
-        }
-
-        
-    }
-}
-
-console.log('El maximo es: ' + maximo);
+Encuentre el valor mínimo de un array usando la función Math.min() en JavaScript
+Encuentre el valor máximo de un array usando la función Math.max() en JavaScript
+Algunos navegadores no admitan el método anterior, por lo que puede usar la función 
+apply() junto con la función Math.min() / Math.max() para obtener el valor
 
 */
+
+function myFunction(){
+      
+    var max = Math.max.apply(null, arr);
+    var min = Math.min.apply(null, arr);
+    return [max, min];
+}
+myFunction();
+
