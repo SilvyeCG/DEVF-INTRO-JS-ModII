2e390400f72a839cb74ca6a35b6e386183716608
silvye <scortesg9527@gmail.com>
2022-06-09 13:55:04 -0500

    Ejercicio 7.4 con metodo swith y functions

--- Actividad7/exercise4.js
+++ Actividad7/exercise4.js
@@ -1,14 +1,51 @@
+/*  
+Crear un programa el cual sirva para convertir temperaturas. De celsius a fahrenheit  y de 
+*/
+/*
+
 function celsius(grades){
     let totalc = grades * 9 / 5.0 + 32;
     return totalc;
 }
 
-function farenheight(grades){
+function fahrenheit(grades){
     let totalf = (grades - 32) * 5 / 9;
     return totalf;
 }
 
 celsius(58);
-farenheight(38);
+fahrenheit(38);
+
+*/
+
+var conversion = prompt('Que función quieres ejecutar: \n a. Celsius a Fahrenheit \n b. Fahrenheit a Celsius');
+var grade = prompt('Coloca los grados a convertir');
+
+switch(conversion){
+
+    case 'a':
+
+        function celsius(grades){
+            let totalc = grades * 9 / 5.0 + 32;
+            return totalc;
+        }
+
+        celsius(grade);
+    break;
+
+    case 'b':
+        function fahrenheit(grades){
+            let totalf = (grades - 32) * 5 / 9;
+            return totalf;
+        }
+
+        fahrenheit(grade);
+    break;
+
+    default:
+        console.log('Invalid option');
+    break;
+}
+
 
 
